00a43901690f21ff5e4115399a6fb0e4e3705a34
nestor-remo <nestiray12345@gmail.com>
2025-03-13 16:49:00 -0700

    start navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,39 @@
+
+<nav class="nav">
+  <a href="/">Home</a>
+  <a href="/ai">AI</a>
+  <a href=/algo>Algo</a>
+  <a href="/design">About</a>
+  <a href="/dev">Dev</a>
+  <a href="game-dev">Game-Dev</a>
+  <a href="/general">General</a>
+  <a href="/icpc">ICPC</a>
+  <a href="/oss">OSS</a>
+</nav>
+
+<main>
+  <slot />
+</main>
+
+<style>
+  .nav {
+    display: flex;
+    gap: 1rem;
+    justify-content: space-evenly;
+    padding: 1rem;
+    margin: 1px solid;
+  }
+
+  .nav a {
+    text-decoration: none;
+    padding: 1rem;
+    color: black;
+    font-size: 1.5rem;
+    border-radius: 25px;
+    background-color: beige;
+  }
+
+  .nav a:hover {
+    background-color: white;
+  }
+</style>
