e430db1e8e87691f16f9ecde8dd279fbb5fe8aa7
adriancancode <vazqueza2004@gmail.com>
2025-04-10 17:16:02 -0700

    Add ACM CSUF logo to landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,24 +12,41 @@
 </svelte:head>
 
 <!-- TODO: Get random images for base route. -->
-<h1>ACM Gallery</h1>
-<p> Here we will store pictures taken by the marketing team!!</p>
-<section class="gallery">
-  {#if galleryItems.length > 0}
-    {#each data.gallery as item (item.id)}
-      <img
-        src="https://drive.google.com/thumbnail?id={item.id}&sz=s1000"
-        title="Embedded Document"
-        alt="img"
-        referrerpolicy="no-referrer"
-      />
-    {/each}
-  {:else}
-  <p>No pictures here yet! 😔 </p>
-{/if}
-</section>
+<body>
+  <section class="header">
+    <img src='acm_logo.png' alt="ACM CSUF logo"/>
+    <h1>ACM Gallery</h1>
+    <p> Here we will store pictures taken by the marketing team!!</p>
+  </section>
+  <section class="gallery">
+    {#if galleryItems.length > 0}
+      {#each data.gallery as item (item.id)}
+        <iframe
+          src="https://drive.google.com/file/d/{item.id}/preview"
+          title="Embedded Document"
+          width="640"
+          height="480"
+          allow="autoplay"
+          loading="lazy"
+        />
+      {/each}
+    {:else}
+    <p>No pictures here yet! 😔 </p>
+  {/if}
+  </section>
+</body>
+
+
+
 
 <style>
+  * {
+    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    color: white;
+  }
+  body {
+    background-color: #0025ac;
+  }
   .gallery {
     display: flex;
     flex-wrap: wrap;
